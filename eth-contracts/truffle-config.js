97242143aaefad6bf6f8af284b26ca9a1652099c
manticorevault <artur_seilah@hotmail.com>
2020-10-17 12:38:05 +0100

    Updated the truffle-config.js file, still can't deploy to the Rinkeby Network

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -1,19 +1,20 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraKey = "e104f8dbb85c47738de878f55c504b19";
-const fs = require('fs');
 const mnemonic = "bronze youth green piano wash tunnel raccoon dawn liquid avocado pudding ship"
-
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 9545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*"
     },
-
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      provider: function () {
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`);
+      },
       network_id: 4,
-    },
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
   }
-}
+};
